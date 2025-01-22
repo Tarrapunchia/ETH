419f7834b2a5b0d414dc5a7a5bcc21a70b30462a
fzucconi <f.zuc87@gmail.com>
2025-01-22 15:58:54 +0100

    messi i contratti di conio NFT

--- contracts/CrowdfundingWithNFTContract.sol
+++ contracts/CrowdfundingWithNFTContract.sol
@@ -1,32 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./CustomNFT.sol";
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-
-struct Campaign {
-        // money collected so far
-        uint256 collectedFunds; // la voglio 256 perche' i token seguono questo range
-        
-        // obiettivo minimo nel founding
-        uint256 minGoalToCollect;
-
-        // va beh...
-        uint256 numOfContributors;
-
-        // timestamp of the campaign end
-        uint256 campaingEndTime;
-
-        // address di chi crea la campagna
-        address adminCampaign;
-
-        // id campaign
-        uint256 idCampaign;
-
-        // mapping chiave-valore dei contribuiti
-        mapping(address => uint256) contribution;
-
-}
 
 contract Crowfunding {
     /// EVENTS
@@ -47,51 +24,68 @@ contract Crowfunding {
         error GoalNotReached();
         error CampaignNotEnded();
         error GoalReached();
+        error InvalidAddress();
 
 
     /// STATE VARIABLES
 
-        // id campaign
-        uint256 idCampaign;
+        // money collected so far
+        uint256 public collectedFunds; // la voglio 256 perche' i token seguono questo range
+        
+        // obiettivo minimo nel founding
+        uint256 public minGoalToCollect;
+
+        // va beh...
+        uint256 public numOfContributors;
+
+        // timestamp of the campaign end
+        uint256 public campaingEndTime;
+
+        // address di chi crea la campagna
+        address public adminCampaign;
 
         // address del tonek USDC nella chain
         address public usdcTokenAddress;
 
+        // address del contratto del token
+        CustomNFT public tokenContract;
+
+        // mapping chiave-valore dei contribuiti
+        mapping(address => uint256) public contribution;
+
         // mapping per campagne multiple
-        mapping(uint256 => Campaign) public Campaigns;
+        mapping(uint256 => mapping(address => uint256)) contribtionByCampaign;
+
+
 
     /// CONTRUCTOR
-        constructor(address _usdcTokenAddress) {
-            usdcTokenAddress = _usdcTokenAddress;
-            // adminCampaign = _adminCampaign;
-            // minGoalToCollect = _minGoalToCollect;
-            // campaingEndTime = _endTime;
+        constructor(uint256 _minGoalToCollect, address _adminCampaign, uint256 _endTime, address _tokenContractAddress) {
+            adminCampaign = _adminCampaign;
+            minGoalToCollect = _minGoalToCollect;
+            campaingEndTime = _endTime;
+            tokenContract = CustomNFT(_tokenContractAddress);
         }
 
 
     /// METHODS
 
-        // init campaign
-        function initCampaign(uint256 _minGoalToCollect, address _adminCampaign, uint256 _endTime) public {
-            Campaigns[idCampaign].adminCampaign = _adminCampaign;
-            Campaigns[idCampaign].campaingEndTime = _endTime;
-            Campaigns[idCampaign].minGoalToCollect = _minGoalToCollect;
-            idCampaign++;
+
+        function setCustomNFTAddress(address _customNFTAddress) public onlyAdmin {
+            if (address(tokenContract) != address(0))
+                revert InvalidAddress();
+            tokenContract = CustomNFT(_customNFTAddress);
         }
 
         // Donations
-        function contribute(uint256 amount, uint256 _idCampaign) public payable {
-
-
-            Campaign storage _buf = Campaigns[_idCampaign];
+        function contribute(uint256 amount) public payable {
             // se la campagna e' finita esco dal metodo (con revert) ritornando un errore custom campaign_ended
-            if (block.timestamp > _buf.campaingEndTime)
+            if (block.timestamp > campaingEndTime)
                 revert CampaignEnded();
             // altermativa si puo fare con require
 
             if (amount == 0)
                 revert InvalidAmount();
-            if (_buf.contribution[msg.sender] > 0)
+            if (contribution[msg.sender] > 0)
                 revert AlreadyDonated();
             // prima del transerFrom il contratto deve chiedere l'approval da parte dell'utente per spendere i soldi
             // richiesti dal contratto (questo funziona solo in direzione del contratto visto che vado ad eseguire
@@ -103,24 +97,24 @@ contract Crowfunding {
             // contratto
 
             // si occupa gia IERC20.metodo di controllare se il tutto funziona ed a mandare gli eventi
-            IERC20(usdcTokenAddress).transferFrom(msg.sender, _buf.adminCampaign, amount);
-            _buf.collectedFunds += amount;
-            _buf.numOfContributors++;
-            _buf.contribution[msg.sender] = amount;
+            IERC20(usdcTokenAddress).transferFrom(msg.sender, adminCampaign, amount);
+            collectedFunds += amount;
+            numOfContributors++;
+            contribution[msg.sender] = amount;
 
+            // chiamo il metodo direttamente, IMPORTANTE CHE INITIAL OWNER SIA SETTATO BENE
+            tokenContract.safeMint(msg.sender);
             emit Contribution(msg.sender, amount);
         }
 
         // Recupero palanche da parte del donatore
-        function withdraw(uint256 _idCampaign) public payable {
-            Campaign storage _buf = Campaigns[_idCampaign];
-
+        function withdraw() public payable {
             // se la campagna e' finita esco dal metodo (con revert) ritornando un errore custom CampaignEnded
-            if (block.timestamp > _buf.campaingEndTime)
+            if (block.timestamp > campaingEndTime)
                 revert CampaignEnded();
             
             // prelevo il valore della quantita' di soldi donati dall'utente delle donazioni
-            uint256 amountDonated = _buf.contribution[msg.sender];
+            uint256 amountDonated = contribution[msg.sender];
 
             // controllo se la donazione totale e' nulla
             if (amountDonated == 0)
@@ -128,55 +122,51 @@ contract Crowfunding {
 
             // la differenza tra transfer e transferFrom e che il primo lo uso se voglio DARE i soldi dal contratto (mi serve solo il to) mentre il secondo
             // se voglio DARE i soldi al contratto (mi serve il from ed il to)
-            IERC20(usdcTokenAddress).transfer(msg.sender, amountDonated);                   
-            _buf.contribution[msg.sender] = 0;
-            _buf.collectedFunds -= amountDonated;
-            _buf.numOfContributors--;
+            IERC20(usdcTokenAddress).transfer(msg.sender, amountDonated);
+            contribution[msg.sender] = 0;
+            collectedFunds -= amountDonated;
+            numOfContributors--;
 
             emit Withdraw(msg.sender, amountDonated);
 
         }
 
         // Prelievo dei soldi da parte dell'admin quando sono stati raggiunti gli obiettivi economici && di tempo
-        function claimFunds(uint256 _idCampaign) public payable onlyAdmin(_idCampaign){
-            Campaign storage _buf = Campaigns[_idCampaign];
-
+        function claimFunds() public payable onlyAdmin{
             // if (msg.sender != adminCampaign)
             //     revert NotAdmin();
-            if (_buf.collectedFunds < _buf.minGoalToCollect)
+            if (collectedFunds < minGoalToCollect)
                 revert GoalNotReached();
-            if (block.timestamp < _buf.campaingEndTime)
+            if (block.timestamp < campaingEndTime)
                 revert CampaignNotEnded();
 
-            IERC20(usdcTokenAddress).transfer(_buf.adminCampaign, _buf.collectedFunds);
+            IERC20(usdcTokenAddress).transfer(adminCampaign, collectedFunds);
 
-            emit ClaimCampaign(msg.sender, _buf.collectedFunds);
+            emit ClaimCampaign(msg.sender, collectedFunds);
         }
 
         // recuper soldi se la campagna e' terminata ma il goal non e' stato raggiunto
-        function emergencyWithdraw(uint256 _idCampaign) public payable {
-            Campaign storage _buf = Campaigns[_idCampaign];
-
-            if (block.timestamp < _buf.campaingEndTime)
+        function emergencyWithdraw() public payable {
+            if (block.timestamp < campaingEndTime)
                 revert CampaignNotEnded();
-            if (_buf.collectedFunds >= _buf.minGoalToCollect)
+            if (collectedFunds >= minGoalToCollect)
                 revert GoalReached();
-            uint256 _amount = _buf.contribution[msg.sender];
+            uint256 _amount = contribution[msg.sender];
             if (_amount == 0)
                 revert NotExistingContribution();
 
-            IERC20(usdcTokenAddress).transferFrom(_buf.adminCampaign, msg.sender, _amount);
-            _buf.collectedFunds -= _amount;
-            _buf.numOfContributors--;
-            _buf.contribution[msg.sender] = 0;
+            IERC20(usdcTokenAddress).transferFrom(adminCampaign, msg.sender, _amount);
+            collectedFunds -= _amount;
+            numOfContributors--;
+            contribution[msg.sender] = 0;
 
             emit EmergencyWithdraw(msg.sender, _amount);
         }
 
     /// MODIFIERS
         // sono una kw di solidity al cui intero va una logica
-        modifier onlyAdmin(uint256 _idCampaign) {
-            if (msg.sender != Campaigns[_idCampaign].adminCampaign)
+        modifier onlyAdmin() {
+            if (msg.sender != adminCampaign)
                 revert NotAdmin();
             _;
         // il modificatore deve terminare con _;
